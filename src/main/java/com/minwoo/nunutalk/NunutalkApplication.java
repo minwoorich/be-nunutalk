99ae8451c0954af13b894b143d6e13dda8efcb1b
minwoorich <minwoorich@naver.com>
2024-06-27 18:22:57 +0900

    init commit

---
+++ src/main/java/com/minwoo/nunutalk/NunutalkApplication.java
@@ -0,0 +1,13 @@
+package com.minwoo.nunutalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunutalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NunutalkApplication.class, args);
+    }
+
+}

2a7ba61e15374951dacc14927b31915083e8aba6
minwoorich <minwoorich@naver.com>
2024-07-16 07:56:39 +0900

    feat:ChatParticipants 엔티티 완성

--- src/main/java/com/minwoo/nunutalk/domain/ChatParticipants.java
+++ src/main/java/com/minwoo/nunutalk/domain/ChatParticipants.java
@@ -1,9 +1,26 @@
 package com.minwoo.nunutalk.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.minwoo.nunutalk.domain.uuid.GeneratedUuid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "chat_participants")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatParticipants {
+    @Id @GeneratedUuid
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }

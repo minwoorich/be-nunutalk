ebfaf24e4e3f19c11f15d7d8a8dd52cb2cbd9832
minwoorich <minwoorich@naver.com>
2024-07-18 19:19:58 +0900

    feat: CreateChatRoomService 생성 (미완성)

--- src/main/java/com/minwoo/nunutalk/service/CreateChatRoomService.java
+++ src/main/java/com/minwoo/nunutalk/service/CreateChatRoomService.java
@@ -1,8 +1,22 @@
 package com.minwoo.nunutalk.service;
 
+import com.minwoo.nunutalk.controller.CreateRoomDto;
+import com.minwoo.nunutalk.domain.ChatRoom;
+import com.minwoo.nunutalk.domain.port.ChatParticipantRepository;
+import com.minwoo.nunutalk.domain.port.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreateChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatParticipantRepository chatParticipantRepository;
+
+    // TODO
+    public ChatRoom create(CreateRoomDto createRoomDto) {
+        return null;
+    }
 
 }

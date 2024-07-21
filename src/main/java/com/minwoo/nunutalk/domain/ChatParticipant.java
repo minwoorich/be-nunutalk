271eebe0c356ef8398ada13b1c63188eb2f4adbd
Jhung Minwoo <minwoorich96@gmail.com>
2024-07-22 08:37:10 +0900

    fix: pk 컬럼명 수정

--- src/main/java/com/minwoo/nunutalk/domain/ChatParticipant.java
+++ src/main/java/com/minwoo/nunutalk/domain/ChatParticipant.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatParticipant extends BaseEntity {
     @Id @GeneratedUuid
+    @Column(name = "chat_participant_id")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)

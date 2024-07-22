ed2cc7ce936d500877d049a82af1d62c16b13bbd
minwoorich <minwoorich@naver.com>
2024-07-22 15:00:25 +0900

    ✨ ChatService : create() 메서드 구현 (채팅방 생성)

--- src/main/java/com/minwoo/nunutalk/service/CreateChatRoomService.java
+++ src/main/java/com/minwoo/nunutalk/service/CreateChatRoomService.java
@@ -1,22 +1,45 @@
 package com.minwoo.nunutalk.service;
 
 import com.minwoo.nunutalk.controller.CreateRoomDto;
+import com.minwoo.nunutalk.domain.ChatParticipant;
 import com.minwoo.nunutalk.domain.ChatRoom;
+import com.minwoo.nunutalk.domain.Member;
+import com.minwoo.nunutalk.domain.enums.ChatRoomState;
 import com.minwoo.nunutalk.domain.port.ChatParticipantRepository;
 import com.minwoo.nunutalk.domain.port.ChatRoomRepository;
+import com.minwoo.nunutalk.domain.port.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static com.minwoo.nunutalk.domain.enums.ChatRoomState.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CreateChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final ChatParticipantRepository chatParticipantRepository;
+    private final MemberRepository memberRepository;
 
-    // TODO
+    @Transactional
     public ChatRoom create(CreateRoomDto createRoomDto) {
-        return null;
+
+        ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.builder().state(ACTIVE).title(createRoomDto.title()).build());
+
+        Member owner = memberRepository.findById(createRoomDto.ownerId());
+        Member friend = memberRepository.findById(createRoomDto.friendId());
+
+        ChatParticipant chatParticipantOwner = ChatParticipant.builder().member(owner).chatRoom(chatRoom).build();
+        ChatParticipant chatParticipantFriend = ChatParticipant.builder().member(friend).chatRoom(chatRoom).build();
+
+        chatParticipantRepository.save(chatParticipantOwner);
+        chatParticipantRepository.save(chatParticipantFriend);
+
+        return chatRoom;
     }
 
 }

5782c1ad4ac8b5623fa89e94feb02b5a42cddcdc
minwoorich <minwoorich@naver.com>
2024-07-22 16:19:38 +0900

    🐛 CreateRoomDto 의 파라미터에서 아예 회원아이디들을 리스트로 받음 - 단체로 인원을 초대할 경우도 있기 때문 - 채팅방은 1:1채팅, 단체 채팅 따로 구분이 없음.

--- src/main/java/com/minwoo/nunutalk/controller/CreateRoomDto.java
+++ src/main/java/com/minwoo/nunutalk/controller/CreateRoomDto.java
@@ -1,6 +1,7 @@
 package com.minwoo.nunutalk.controller;
 
+import java.util.List;
 import java.util.UUID;
 
-public record CreateRoomDto(String title, UUID ownerId, UUID friendId) {
+public record CreateRoomDto(String title, List<UUID> members) {
 }

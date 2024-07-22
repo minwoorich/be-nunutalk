616d7ccb8f8784270317675ca7750945103bdbcd
minwoorich <minwoorich@naver.com>
2024-07-22 16:58:40 +0900

    🐛 CreateRoomDto 에 @Builder 추가

--- src/main/java/com/minwoo/nunutalk/controller/CreateRoomDto.java
+++ src/main/java/com/minwoo/nunutalk/controller/CreateRoomDto.java
@@ -1,7 +1,10 @@
 package com.minwoo.nunutalk.controller;
 
+import lombok.Builder;
+
 import java.util.List;
 import java.util.UUID;
 
-public record CreateRoomDto(String title, List<UUID> members) {
+@Builder
+public record CreateRoomDto(String title, List<UUID> memberIds) {
 }

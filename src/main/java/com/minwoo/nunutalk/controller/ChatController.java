7fe4b0d9cdd24e358ed85ab9e5384e8760004539
minwoorich <minwoorich@naver.com>
2024-07-22 19:21:00 +0900

    ✨ 회원이 참가하고있는 모든 채팅방 조회하는 API 추가(미완)
    - 외부로 내보내는 DTO 하나 따로 만들어야함

--- src/main/java/com/minwoo/nunutalk/controller/ChatController.java
+++ src/main/java/com/minwoo/nunutalk/controller/ChatController.java
@@ -1,37 +1,53 @@
 package com.minwoo.nunutalk.controller;
 
+import com.minwoo.nunutalk.annotations.ApiV1Controller;
 import com.minwoo.nunutalk.domain.ChatMessage;
+import com.minwoo.nunutalk.domain.ChatRoom;
 import com.minwoo.nunutalk.service.ChatService;
+import com.minwoo.nunutalk.service.CreateChatRoomService;
+import com.minwoo.nunutalk.service.GetChatRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
-@RestController
 @RequiredArgsConstructor
+@ApiV1Controller
 public class ChatController {
 
     private final SimpMessagingTemplate template;
     private final ChatService chatService;
+    private final CreateChatRoomService createChatRoomService;
+    private final GetChatRoomService getChatRoomService;
 
-    @PostMapping("/chatroom")
-    public void createRoom(@RequestBody CreateRoomDto createRoomDto){
-
+    @PostMapping("/chatrooms")
+    public ResponseEntity<ChatRoom> createRoom(@RequestBody CreateRoomDto createRoomDto){
+        return ResponseEntity.ok(createChatRoomService.create(createRoomDto));
     }
-    @SubscribeMapping("/message")
-    public void join(Message<SubsMessageDto> message) {
 
+    @GetMapping("/chatrooms/{memberId}")
+    public ResponseEntity<List<ChatRoom>> getChatRooms(@PathVariable(name = "memberId") UUID memberId){
+        return ResponseEntity.ok(getChatRoomService.findAllChatRooms(memberId));
     }
-    @MessageMapping("/message")
+
+    @MessageMapping("/messages")
     public void send(Message<SendMessageDto> message) {
         template.convertAndSend("/chat-room",message.getPayload());
         ChatMessage savedMessage = chatService.saveMessage(message.getPayload());
         log.info("savedMessage:{}",savedMessage);
     }
+
+    // TODO
+    static record GetChatRoomDto(String roomId, String memberId) {}
+
 }

04adfdb65465cae04eea1583cdf5a030a0b3e5aa
minwoorich <minwoorich@naver.com>
2024-07-23 12:03:27 +0900

    🐛 환경변수 파일 .env 파일 생성
    - 앞으로 .env 파일로 properties 값들을 관리
    - .gitignore 에 .env 파일 등록
    - dotenv 라이브러리 추가

--- src/main/java/com/minwoo/nunutalk/NunutalkApplication.java
+++ src/main/java/com/minwoo/nunutalk/NunutalkApplication.java
@@ -1,5 +1,6 @@
 package com.minwoo.nunutalk;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NunutalkApplication {
 
     public static void main(String[] args) {
+
+        // 1) .env 파일 로드
+        Dotenv dotenv = Dotenv.configure().load();
+        // 2) .env 파일에 적힌 properties 들을 현재 시스템에 적용
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(NunutalkApplication.class, args);
     }
 

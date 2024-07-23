20f27ed7ae62d9cc7d6f5d66d2dd7ff6a00f27d2
minwoorich <minwoorich@naver.com>
2024-07-23 14:20:33 +0900

    🐛 생성일자, 업뎃일자 자동으로 insert 안되는 오류 해결
    @EnableJpaAuditing 추가 해야함

--- src/main/java/com/minwoo/nunutalk/NunutalkApplication.java
+++ src/main/java/com/minwoo/nunutalk/NunutalkApplication.java
@@ -3,8 +3,10 @@ package com.minwoo.nunutalk;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NunutalkApplication {
 
     public static void main(String[] args) {

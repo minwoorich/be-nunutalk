b1903fca9de4208d841fc8b874e3cb55ffe9a0ed
minwoorich <minwoorich@naver.com>
2024-07-23 10:47:33 +0900

    ✨ GetMemberChatRoomsDto : 컨버팅 메서드 추가
    - from() : ChatRoom엔티티 -> ChatRoomInfo 로 변환

---
+++ src/main/java/com/minwoo/nunutalk/controller/MemberChatRoomsResp.java
@@ -0,0 +1,25 @@
+package com.minwoo.nunutalk.controller;
+
+import com.minwoo.nunutalk.domain.ChatRoom;
+import com.minwoo.nunutalk.domain.enums.ChatRoomState;
+import lombok.Builder;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+// TODO : filter 파라미터
+@Builder
+public record MemberChatRoomsResp(UUID memberId, List<ChatRoomInfo> chatRoomInfos) {
+    @Builder
+    public record ChatRoomInfo(UUID roomId, ChatRoomState state, String title, ZonedDateTime createdAt){
+        public ChatRoomInfo from(ChatRoom chatRoom){
+            return ChatRoomInfo.builder()
+                    .roomId(chatRoom.getId())
+                    .state(chatRoom.getState())
+                    .title(chatRoom.getTitle())
+                    .createdAt(chatRoom.getCreatedAt())
+                    .build();
+        }
+    }
+}

2b618667d5b97d4485d1fb54c77fa056464b04cf
minwoorich <minwoorich@naver.com>
2024-07-24 17:44:17 +0900

    ⬆️ @NonNull -> @Size 로 유효성 검사 변경

--- src/main/java/com/minwoo/nunutalk/api/dto/CreateRoomReq.java
+++ src/main/java/com/minwoo/nunutalk/api/dto/CreateRoomReq.java
@@ -1,6 +1,7 @@
 package com.minwoo.nunutalk.api.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.NonNull;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @Builder
 public record CreateRoomReq(
-        @Schema(description = "채팅방 제목", example = "독서 스터디 모임") @NonNull String title,
-        @Schema(description = "채팅방에 참여하는 모든 인원의 ID", example = "[\"0190d945-440c-7ccf-a688-0eb70af456a5\", \"0190d945-440c-7ccf-a688-0eb70ad426a5\"]") @NonNull List<UUID> memberIds) {
+        @Schema(description = "채팅방 제목", example = "독서 스터디 모임") @Size(min = 1, max = 30) String title,
+        @Schema(description = "채팅방에 참여하는 모든 인원의 ID", example = "[\"0190d945-4424-7bee-af99-3a83d52985cb\", \"0190d945-440c-7ccf-a688-0eb70ad426a5\"]") @NonNull List<UUID> memberIds) {
 }
+

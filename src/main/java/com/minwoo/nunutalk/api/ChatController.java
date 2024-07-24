d46e79f4f6bff26cbb0d42f39445311b24cfb0fe
minwoorich <minwoorich@naver.com>
2024-07-24 12:07:07 +0900

    🩹 컨트롤러 이름 변경
    - IChatController -> ChatController (인터페이스)
    - ChatController -> ChatControllerImpl (구현체)

--- src/main/java/com/minwoo/nunutalk/api/ChatController.java
+++ src/main/java/com/minwoo/nunutalk/api/ChatController.java
@@ -4,57 +4,29 @@ import com.minwoo.nunutalk.api.dto.CreateRoomReq;
 import com.minwoo.nunutalk.api.dto.MemberChatRoomsResp;
 import com.minwoo.nunutalk.api.dto.SendMessageDto;
 import com.minwoo.nunutalk.common.CustomResponse;
-import com.minwoo.nunutalk.common.annotations.ApiV1Controller;
-import com.minwoo.nunutalk.domain.ChatMessage;
-import com.minwoo.nunutalk.service.ChatService;
-import com.minwoo.nunutalk.service.CreateChatRoomService;
-import com.minwoo.nunutalk.service.GetChatRoomService;
-import lombok.extern.slf4j.Slf4j;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
 import java.util.UUID;
 
-@Slf4j
-@ApiV1Controller
-public class ChatController implements IChatController{
+@Tag(name = "Chat", description = "채팅")
+public interface ChatController {
+    @Operation(summary = "채팅방 생성", description = "채팅방을 오픈할 때 호출하는 API")
+    @PostMapping("/chatrooms")
+    CustomResponse<?> createChatRoom(@RequestBody CreateRoomReq createRoomReq);
 
-    private final SimpMessagingTemplate template;
-    private final ChatService chatService;
-    private final CreateChatRoomService createChatRoomService;
-    private final GetChatRoomService getChatRoomService;
+    @Operation(summary = "채팅방 불러오기", description = "회원이 참가해있는 모든 채팅방 정보를 불러온다")
+    @Parameter(name = "memberId", example = "0190d945-4424-7bee-af99-3a83d52985cb")
+    @GetMapping("/chatrooms/{memberId}")
+    CustomResponse<MemberChatRoomsResp> getChatRooms(@PathVariable(name = "memberId") UUID memberId);
 
-    public ChatController(SimpMessagingTemplate template, ChatService chatService, CreateChatRoomService createChatRoomService, GetChatRoomService getChatRoomService) {
-        this.template = template;
-        this.chatService = chatService;
-        this.createChatRoomService = createChatRoomService;
-        this.getChatRoomService = getChatRoomService;
-    }
-
-    @Override
-    public CustomResponse<?> createChatRoom(CreateRoomReq createRoomReq){
-        createChatRoomService.create(createRoomReq);
-        return CustomResponse.create();
-    }
-
-    @Override
-    public CustomResponse<MemberChatRoomsResp> getChatRooms(UUID memberId){
-        return CustomResponse.ok(MemberChatRoomsResp.create(memberId, getChatRoomInfos(memberId)));
-    }
-
-    private List<MemberChatRoomsResp.ChatRoomInfo> getChatRoomInfos(UUID memberId) {
-        return getChatRoomService
-                .findAllChatRooms(memberId).stream()
-                .map(MemberChatRoomsResp.ChatRoomInfo::from)
-                .toList();
-    }
-
-    @Override
-    public void send(Message<SendMessageDto> message) {
-        template.convertAndSend("/chat-room",message.getPayload());
-        ChatMessage savedMessage = chatService.saveMessage(message.getPayload());
-        log.info("savedMessage:{}",savedMessage);
-    }
-
+    @MessageMapping("/messages")
+    void send(Message<SendMessageDto> message);
 }
---
+++ src/main/java/com/minwoo/nunutalk/api/ChatControllerImpl.java
@@ -0,0 +1,60 @@
+package com.minwoo.nunutalk.api;
+
+import com.minwoo.nunutalk.api.dto.CreateRoomReq;
+import com.minwoo.nunutalk.api.dto.MemberChatRoomsResp;
+import com.minwoo.nunutalk.api.dto.SendMessageDto;
+import com.minwoo.nunutalk.common.CustomResponse;
+import com.minwoo.nunutalk.common.annotations.ApiV1Controller;
+import com.minwoo.nunutalk.domain.ChatMessage;
+import com.minwoo.nunutalk.service.ChatService;
+import com.minwoo.nunutalk.service.CreateChatRoomService;
+import com.minwoo.nunutalk.service.GetChatRoomService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@ApiV1Controller
+public class ChatControllerImpl implements ChatController {
+
+    private final SimpMessagingTemplate template;
+    private final ChatService chatService;
+    private final CreateChatRoomService createChatRoomService;
+    private final GetChatRoomService getChatRoomService;
+
+    public ChatControllerImpl(SimpMessagingTemplate template, ChatService chatService, CreateChatRoomService createChatRoomService, GetChatRoomService getChatRoomService) {
+        this.template = template;
+        this.chatService = chatService;
+        this.createChatRoomService = createChatRoomService;
+        this.getChatRoomService = getChatRoomService;
+    }
+
+    @Override
+    public CustomResponse<?> createChatRoom(CreateRoomReq createRoomReq){
+        createChatRoomService.create(createRoomReq);
+        return CustomResponse.create();
+    }
+
+    @Override
+    public CustomResponse<MemberChatRoomsResp> getChatRooms(UUID memberId){
+        return CustomResponse.ok(MemberChatRoomsResp.create(memberId, getChatRoomInfos(memberId)));
+    }
+
+    private List<MemberChatRoomsResp.ChatRoomInfo> getChatRoomInfos(UUID memberId) {
+        return getChatRoomService
+                .findAllChatRooms(memberId).stream()
+                .map(MemberChatRoomsResp.ChatRoomInfo::from)
+                .toList();
+    }
+
+    @Override
+    public void send(Message<SendMessageDto> message) {
+        template.convertAndSend("/chat-room",message.getPayload());
+        ChatMessage savedMessage = chatService.saveMessage(message.getPayload());
+        log.info("savedMessage:{}",savedMessage);
+    }
+
+}
